055e7eb01214b5e7c4aef278aab708df662df6a5
netang <netang@yandex.ru>
2020-04-04 13:51:40 +0300

    Remove React.StrictMode

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import axios from 'axios';
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
